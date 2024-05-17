c0534957b912e97e084590afed2470376c3a6b5a
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-17 21:32:59 +0300

    post

--- WebApplication2/Controllers/TodosController.cs
+++ WebApplication2/Controllers/TodosController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WebApplication2.Dtos;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
@@ -15,11 +19,33 @@ namespace WebApplication2.Controllers
             HttpClient http = new();
             string uri = " https://jsonplaceholder.typicode.com/todos";
             HttpResponseMessage htttpErrorResponse = await http.GetAsync(uri);
-            if(htttpErrorResponse.IsSuccessStatusCode) 
-            { 
-              List<Todo>? todos=await htttpErrorResponse.Content.ReadFromJsonAsync<List<Todo>>();            
+            if (htttpErrorResponse.IsSuccessStatusCode)
+            {
+                List<Todo>? todos = await htttpErrorResponse.Content.ReadFromJsonAsync<List<Todo>>();
+                if (todos is not null)
+                {
+                    return Ok(todos);
+                }
             }
             return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTodoDto request)
+        {
+            string uri = " https://jsonplaceholder.typicode.com/todos";
+            string content = JsonSerializer.Serialize(request);
+            StringContent stringContent = new(content);
+            HttpClient http = new();
+            HttpResponseMessage message = await http.PostAsync(uri, stringContent);
+            if (message.IsSuccessStatusCode)
+            {
+                Todo? todo = await message.Content.ReadFromJsonAsync<Todo>();
+                if (todo is not null)
+                {
+                    return Ok(todo);
+                }
+            }
+            return BadRequest(new { Message = "Something went wong...." });
         }
     }
 }
---
+++ WebApplication2/Dtos/CreateTodoDto.cs
@@ -0,0 +1,5 @@
+﻿namespace WebApplication2.Dtos;
+
+public sealed record CreateTodoDto(string Title,bool Completed,int UserId);
+
+
