531da2aaa993530148c3c1ef8061b397c94ada76
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-17 20:48:49 +0300

    .

--- WebApplication2/Controllers/TodosController.cs
+++ WebApplication2/Controllers/TodosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
 
 namespace WebApplication2.Controllers
 {
@@ -7,9 +8,17 @@ namespace WebApplication2.Controllers
     [ApiController]
     public class TodosController : ControllerBase
     {
+        // https://jsonplaceholder.typicode.com/todos
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
+            HttpClient http = new();
+            string uri = " https://jsonplaceholder.typicode.com/todos";
+            HttpResponseMessage htttpErrorResponse = await http.GetAsync(uri);
+            if(htttpErrorResponse.IsSuccessStatusCode) 
+            { 
+              List<Todo>? todos=await htttpErrorResponse.Content.ReadFromJsonAsync<List<Todo>>();            
+            }
             return Ok();
         }
     }
