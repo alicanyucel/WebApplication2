d7b3bb41c927461b88616c80f31beeba272730e4
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-17 20:40:40 +0300

    model

--- WebApplication2/Controllers/TodosController.cs
+++ WebApplication2/Controllers/TodosController.cs
@@ -3,9 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication2.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class TodosController : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
     }
 }
---
+++ WebApplication2/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public sealed class Todo
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }=string.Empty;
+        public bool Completed { get; set; }
+    }
+}
